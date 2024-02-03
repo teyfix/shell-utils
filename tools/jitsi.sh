0cf0afa2ce251cec73a4f3a662e53e52ebd6fe85
Halil Teyfik Dolmaci <hlltvfk@gmail.com>
2024-02-03 21:20:19 +0300

    Refactor file retrieval and add code editor functionality
    This commit refactors the `prosody-conf` and `meet-conf` functions in `jitsi.sh` to improve file retrieval. It now creates temporary files and uses them to store the retrieved files from the prosody container. Additionally, the code editor functionality has been added to open the retrieved files for editing.

--- tools/jitsi.sh
+++ tools/jitsi.sh
@@ -1,9 +1,13 @@
 # retrieves the prosody.cfg.lua file from the prosody container
 prosody-conf() {
-  kube-exec prosody cat /config/prosody.cfg.lua | trim-nf
+  tmp_file=$(mktemp /tmp/prosody.cfg-XXXXXX.lua)
+  kube-exec prosody cat /config/prosody.cfg.lua | trim-nf >$tmp_file
+  code $tmp_file
 }
 
 # retrieves the jitsi-meet.cfg.lua file from the prosody container
 meet-conf() {
-  kube-exec prosody cat /config/conf.d/jitsi-meet.cfg.lua | trim-nf
+  tmp_file=$(mktemp /tmp/jitsi-meet.cfg-XXXXXX.lua)
+  kube-exec prosody cat /config/conf.d/jitsi-meet.cfg.lua | trim-nf >$tmp_file
+  code $tmp_file
 }
