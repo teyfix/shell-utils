1900ebfc259fef2ee578f10d8706e6ef0c9402f7
Halil Teyfik Dolmaci <hlltvfk@gmail.com>
2024-02-03 21:12:35 +0300

    Add meet-conf function to retrieve prosody.cfg.lua file from the prosody container

--- tools/jitsi.sh
+++ tools/jitsi.sh
@@ -1,3 +1,8 @@
+# retrieves the prosody.cfg.lua file from the prosody container
+meet-conf() {
+  kube-exec prosody cat /config/prosody.cfg.lua | trim-nf
+}
+
 # retrieves the jitsi-meet.cfg.lua file from the prosody container
 meet-conf() {
   kube-exec prosody cat /config/conf.d/jitsi-meet.cfg.lua | trim-nf
