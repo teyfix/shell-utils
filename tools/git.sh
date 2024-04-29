ffb3e2f56272d2b9ebc9ef36b2e42ab7a26a9af7
Halil Teyfik Dolmaci <hlltvfk@gmail.com>
2024-04-29 18:21:20 +0300

    added safe-push

--- tools/git.sh
+++ tools/git.sh
@@ -15,13 +15,27 @@ commit-all() {
   commit "$@"
 }
 
+safe-push() {
+  local git_origin="$(git remote)"
+  local git_branch="$(git branch --show-current)"
+
+  # Check if the remote branch exists
+  if git ls-remote --heads "$git_origin" "$git_branch" | grep -q "$git_branch"; then
+    echo "Remote branch exists. Pushing changes..."
+    git push "$git_origin" "$git_branch"
+  else
+    echo "Remote branch does not exist. Pushing and setting upstream..."
+    git push -u "$git_origin" "$git_branch"
+  fi
+}
+
 push() {
-  git push
+  safe-push
 }
 
 push-all() {
   commit-all "$@"
-  git push
+  safe-push
 }
 
 trello() {
