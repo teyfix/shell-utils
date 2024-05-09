389b399135322d6d05135aaee4bbb6bec256feef
Halil Teyfik <hlltvfk@gmail.com>
2024-05-09 14:03:53 +0000

    added ssh-iproute toexclude existing ssh traffic from wireguard etc

---
+++ tools/ssh.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+ssh-iproute() {
+  local traffic="$(ss -n sport 22 | tail -n+2 | grep -Po '\d+(\.\d+)+' | awk 'NR%2==0')"
+
+  for ip in $traffic; do
+    local ip_addr_after_via="$(ip route get "$ip" sport 22 | grep -Po '(?<=via )\d+(\.\d+)+((?! src).)+')"
+    local new_ip_rule="$ip via $ip_addr_after_via"
+    local old_ip_rule="$(ip route list | grep "$new_ip_rule")"
+
+    if [ -z "$old_ip_rule" ]; then
+      cat <<EOF
+Adding missing rule: $new_ip_rule
+EOF
+
+      bash -c "sudo ip route add $new_ip_rule"
+    else
+      cat <<EOF
+Skipping existing rule: $old_ip_rule
+EOF
+    fi
+  done
+}
