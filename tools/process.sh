58c3e6b2f40062af8bd439becb2483ddca7d3051
Halil Teyfik Dolmaci <hlltvfk@gmail.com>
2024-06-20 22:06:03 +0000

    fix(process): process ids overlapping

--- tools/process.sh
+++ tools/process.sh
@@ -4,6 +4,7 @@ show-node() {
 
 kill-node() {
   local processes="$(show-node | awk '{print $2}')"
+  local user_processes="$(show-node | awk '{print $1,$2}')"
 
   if [ -z "$processes" ]; then
     echo "No process found"
@@ -11,7 +12,22 @@ kill-node() {
   fi
 
   local count="$(echo "$processes" | wc -l)"
+  local need_sudo=false
 
-  echo "Killing $count process(es): $processes"
-  echo "$processes" | xargs -r sudo kill -9
+  while IFS= read -r line; do
+    local user=$(echo $line | awk '{print $1}')
+    local pid=$(echo $line | awk '{print $2}')
+    if [ "$user" != "$(whoami)" ]; then
+      need_sudo=true
+      break
+    fi
+  done <<<"$user_processes"
+
+  echo "Killing $count process(es): $(echo -n "$processes" | tr '\n' ',' | sed 's#,#, #')"
+
+  if $need_sudo; then
+    echo "$processes" | xargs -r sudo kill -9
+  else
+    echo "$processes" | xargs -r kill -9
+  fi
 }
