f48bc3ef9259af9e7ccffd559ca7dce758afc863
Halil Teyfik <hlltvfk@gmail.com>
2024-08-29 17:49:49 +0300

    chore: Refactor syncapp function to handle destroy option

--- tools/helmfile.sh
+++ tools/helmfile.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 syncapp() {
-  helmfile destroy -lapp="$1" --skip-deps --debug
-  helmfile sync -lapp="$1" --skip-deps --debug
+  local app="$@"
+
+  if [[ "$app" = *"-d"* || "$app" = *"--destroy"* ]]; then
+    app="$(echo "$app" | tr -s '[:space:]' '\n' | grep -v '\-d' | xargs -n1 -I% echo -lapp=% | xargs)"
+    echo "$app" | xargs -I% helmfile destroy % --skip-deps --debug
+  fi
+
+  echo "$app" | xargs -I% helmfile sync % --skip-deps --debug
 }
